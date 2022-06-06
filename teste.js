763d0c82ad10be4e8f5b649d5a743ec4c2422083
Pedro Barossi <pedro3barossi@gmail.com>
2022-06-06 07:41:58 -0300

    Aulas 1 e 2

---
+++ teste.js
@@ -0,0 +1,19 @@
+// var y = 0; comentário de uma linha
+
+/*var x = 1;
+var z=  2;
+comentário de várias linhas*/
+
+//var preco = 2;
+var desconto = 0.2;
+const PRECO = 2;
+
+//preco = preco - desconto;
+var total = PRECO - desconto;
+
+function soma(a, b) {
+    //console.log(a + b);
+    return a + b;
+}
+
+soma(3, 5);
